c80ce555ff0d1599248e4ac30f374802a4b64d64
Ian Fleming <iantfleming1992@gmail.com>
2020-11-27 11:34:35 +0000

    expect dice roll

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,6 +4,8 @@ describe Dice do
   it { is_expected.to respond_to(:roll)}
 
   it 'rolls a random number' do
-
+    dice = Dice.new
+    expect(subject.roll).to satisfy { |i| i == 1..6 }
+  end
 
 end
